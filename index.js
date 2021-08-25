671fe6f08d111cd7b29f03ece5fafba49e379fda
Lucas Bueno <lucas.bueno@maisinteligencia.com.br>
2021-08-25 11:33:41 -0300

    Create initial script

---
+++ index.js
@@ -0,0 +1,158 @@
+var axios = require('axios');
+var qs = require('qs');
+var FormData = require('form-data');
+var xlsx = require('node-xlsx');
+
+var request = require('request');
+
+
+const IdProjetoMultiB = 1022;
+const IdProjetoAlmoco = 1041;
+
+const ActivityIDMultiB = 39;
+const ActivityIDAlmoco = 10;
+
+let kimayKey;
+
+const ReadExcel = async () => {
+  var obj = xlsx.parse(__dirname + '/apontamento.xlsx'); // parses a file
+
+  var objApontamentos = obj[0].data;
+
+  //console.log(objApontamentos);
+
+  //var obj = xlsx.parse(fs.readFileSync(__dirname + '/apontamento.xls')); // parses a buffer
+
+  var apontamentos = [];
+  for(var i=0; i < objApontamentos.length; i++){
+    if(objApontamentos[i].length > 1) 
+      apontamentos.push(objApontamentos[i]);
+  }
+
+  console.log(apontamentos);
+  
+  for(var i=0; i < apontamentos.length; i++){
+    var day = apontamentos[i][0].replace('/', '.').replace('/', '.');
+    
+    for(var j=1; j < apontamentos[i].length - 1; j++){
+      
+      var entrada = apontamentos[i][j];
+      var saida = apontamentos[i][j + 1];
+
+      var isAlmoco = (j == 2);
+
+      var total = j / 2;
+      var ignore = total % 2 == 0 ? true : false;
+
+      if(ignore && !isAlmoco){
+        continue;
+      }
+
+      idProj = isAlmoco ? IdProjetoAlmoco : IdProjetoMultiB;
+      idActv = isAlmoco ? ActivityIDAlmoco : ActivityIDMultiB;
+
+      let appointment = KimaiAppoint(idProj, idActv, entrada, saida, day);
+      let response_body = await appointment;
+
+      console.log(response_body);
+
+      console.log(`Apontou proj ${idProj} act ${idActv} || ${day} das ${entrada} as ${saida}`)
+    }
+    
+  }
+}
+
+const KimaiLogin = async () => {
+
+  console.log('Fazendo login...');
+
+  var options = {
+    'method': 'POST',
+    'url': 'https://timetracking.knapp.com.br/index.php?a=checklogin',
+    'headers': {
+      'Cookie': 'kimai_key=iH7M0VTyLSzIWeuherk1VtfGGGPFM2; kimai_user=lucas.bueno'
+    },
+    formData: {
+      'name': 'lucas.bueno',
+      'password': '1l2u3c4a5s'
+      }
+  };
+  await request(options, async function (error, response) {
+    if (error) console.log('exception:' + error);
+
+    kimayKey = response.rawHeaders.filter(name => name.includes('kimai_key='));
+
+    console.log('Logado!');
+
+    await ReadExcel();
+  });
+}
+
+const KimaiAppoint = (idproj, activity, startTime, endTime, day) => {
+
+  return new Promise(async (resolve, reject) => {
+    var options = {
+      'method': 'POST',
+      'url': 'https://timetracking.knapp.com.br/extensions/ki_timesheets/processor.php',
+      'headers': {
+        'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
+        'Accept': '*/*',
+        'X-Requested-With': 'XMLHttpRequest',
+        'sec-ch-ua-mobile': '?0',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'Cookie': `${kimayKey}; kimai_user=lucas.bueno`
+      },
+      form: {
+        'activityID': activity,
+        'approved': '',
+        'axAction': 'add_edit_timeSheetEntry',
+        'billable': '0',
+        'budget': '',
+        'comment': '',
+        'commentType': '0',
+        'description': '',
+        'duration': '00:00:00',
+        'end_day': day,//'21.08.2021',
+        'end_time': endTime, //12:00:00
+        'filter': '',
+        'filter': '',
+        'id': '',
+        'location': '',
+        'projectID': idproj,
+        'start_day': day,//'21.08.2021',
+        'start_time': startTime, //08:00:00
+        'statusID': '1',
+        'trackingNumber': '',
+        'userID[]': '932335395' 
+      }
+    };
+    
+    await request(options, function (error, response) {
+      if (error) throw new Error(error);
+      resolve(response.body);
+    });
+  });
+}
+
+/*const parseCookies = (request) => {
+  var list = {},
+      rc = request.headers.cookie;
+
+  rc && rc.split(';').forEach(function( cookie ) {
+      var parts = cookie.split('=');
+      list[parts.shift().trim()] = decodeURI(parts.join('='));
+  });
+
+  return list;
+}*/
+
+KimaiLogin();
+
+/*setInterval(async ()=>{
+    
+    //get data from ponto rep
+    //await processTrade();
+
+}, 1000);*/
+
