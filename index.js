1b986b738fcd2f5e22255df312161e3482b8c3c2
Lucas Bueno <lucas.bueno@maisinteligencia.com.br>
2021-08-26 19:51:20 -0300

    Appointment improvements to check userid dynamically after login

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ let kimayKey;
 
 let user = '';
 let password = '';
+let userID = '';
 
 const ReadExcel = async () => {
   var obj = xlsx.parse(__dirname + '/apontamento.xlsx'); // parses a file
@@ -28,8 +29,15 @@ const ReadExcel = async () => {
   let login = KimaiLogin();
   let response_login = await login;
 
-  console.log(response_login, kimayKey);
+  console.log(kimayKey);
 
+  await GetUserId();
+
+  if (!isNumeric(userID)){
+    console.log('Login inválido');
+    return;
+  }
+  
   var apontamentos = [];
   for(var i=5; i < objApontamentos.length; i++){
     if(objApontamentos[i].length > 1) 
@@ -61,12 +69,46 @@ const ReadExcel = async () => {
       let appointment = KimaiAppoint(idProj, idActv, entrada, saida, day);
       let response_body = await appointment;
 
-      console.log(response_body);
-
-      console.log(`Apontou proj ${idProj} act ${idActv} || ${day} das ${entrada} as ${saida}`)
+      console.log(`Apontou proj ${idProj} act ${idActv} || ${day} das ${entrada} as ${saida} ||`, response_body)
     }
     
   }
+}
+
+function isNumeric(str) {
+  if (typeof str != "string") return false // we only process strings!  
+  return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+         !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+}
+
+const GetUserId = () => {
+  return new Promise(async (resolve, reject) => {
+    var options = {
+      'method': 'GET',
+      'url': 'https://timetracking.knapp.com.br/core/kimai.php',
+      'headers': {
+        'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
+        'sec-ch-ua-mobile': '?0',
+        'Upgrade-Insecure-Requests': '1',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'Cookie': `${kimayKey}; kimai_user=${user}`
+      }
+    };
+    request(options, function (error, response) {
+      if (error) userID = '';
+
+      var findKey = "userID = ";
+      var index = response.body.indexOf(findKey);
+
+      var firstDotPos = index + findKey.length;
+      var id = response.body.substring(firstDotPos, firstDotPos + 9);
+      console.log('userid: ', id);
+
+      userID = id;
+      resolve(userID);
+    });
+  });
 }
 
 const KimaiLogin = async () => {
@@ -75,18 +117,18 @@ const KimaiLogin = async () => {
       'method': 'POST',
       'url': 'https://timetracking.knapp.com.br/index.php?a=checklogin',
       'headers': {
-        'Cookie': 'kimai_key=iH7M0VTyLSzIWeuherk1VtfGGGPFM2; kimai_user=lucas.bueno'
+        'Cookie': `kimai_key=iH7M0VTyLSzIWeuherk1VtfGGGPFM2; kimai_user=${user}`
       },
       formData: {
         'name': user,
         'password': password
         }
     };
-    await request(options, async function (error, response) {
+    await request(options, function (error, response, body) {
       if (error) console.log('exception:' + error);
 
       kimayKey = response.rawHeaders.filter(name => name.includes('kimai_key='));
-      resolve('Logado!');
+      resolve(body);
     });
   })
 }
@@ -104,7 +146,7 @@ const KimaiAppoint = (idproj, activity, startTime, endTime, day) => {
         'sec-ch-ua-mobile': '?0',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36',
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-        'Cookie': `${kimayKey}; kimai_user=lucas.bueno`
+        'Cookie': `${kimayKey}; kimai_user=${user}`
       },
       form: {
         'activityID': activity,
@@ -127,7 +169,7 @@ const KimaiAppoint = (idproj, activity, startTime, endTime, day) => {
         'start_time': startTime, //08:00:00
         'statusID': '1',
         'trackingNumber': '',
-        'userID[]': '932335395' 
+        'userID[]': userID
       }
     };
     
